fff0bc6b9ca19925931efc2754a9cddfe5c316b4
Clowrid <clowrid@gmail.com>
2018-09-26 16:13:23 +0200

    Bouton analyse à la place de l'url dans le message pour le MACD

--- src/model/Bot.ts
+++ src/model/Bot.ts
@@ -16,38 +16,6 @@ export class Bot {
   private cronJob: CronJob;
 
   // private bot: SlackBot;
-  private static params = {
-      attachments: [
-          {
-              "icon_url" : "https://www.bruno-faugeroux.fr/images/catia.png",
-              "fallback": "TODO",
-              "color": "#3AA3E3",
-              "actions": [
-                  {
-                      "text": "Acheter 1 eth",
-                      "type": "button",
-                      "url": "https://www.cat-amania.com",
-                      "confirm": {
-                          "title": "Confirmer l'achat",
-                          "text": "Êtes-vous vraiment sur de vouloir acheter 1 eth ?",
-                          "ok_text": "J'en suis sur",
-                          "dismiss_text": "Annuler"
-                      }
-                  },
-                  {
-                      "text": "Ventre 1 eth",
-                      "type": "button",
-                      "url": "https://www.google.fr",
-                      "confirm": {
-                          "title": "Confirmer la vente",
-                          "text": "Êtes-vous vraiment sur de vouloir vendre 1 eth ?",
-                          "ok_text": "J'en suis sur",
-                          "dismiss_text": "Annuler"
-                      }
-                  }
-              ]
-          }]
-  };
 
   constructor(slackUser: string, currencyPair: string, intervalle: string) {
     this._id = Bot.globalId++;
@@ -63,7 +31,7 @@ export class Bot {
 
   public start() {
     let bot = new SlackBot({
-      token: "xoxb-xxxx", // Add a bot https://my.slack.com/services/new/bot and put the token
+      token: "xoxb-4769124056-379431650867-FPIsyZfuDHtu8G8LiKw7gXMI", // Add a bot https://my.slack.com/services/new/bot and put the token
       name: Bot.botName
     });
     let krakenRequest = new krakenPublicRequest.KrakenPublicRequest();
@@ -75,6 +43,44 @@ export class Bot {
     let urlGraph = "http://78.212.193.11:8182/macd/?grain=" + this.intervalle;
     let talker = new Talk();
     let icon_url = 'icon_url = "https://www.bruno-faugeroux.fr/images/catia.png"';
+
+    let params = {
+          attachments: [
+              {
+                  "icon_url" : "https://www.bruno-faugeroux.fr/images/catia.png",
+                  "fallback": "TODO",
+                  "color": "#3AA3E3",
+                  "actions": [
+                      {
+                          "text": "Acheter 1 eth",
+                          "type": "button",
+                          "url": "https://www.cat-amania.com",
+                          "confirm": {
+                              "title": "Confirmer l'achat",
+                              "text": "Êtes-vous vraiment sur de vouloir acheter 1 eth ?",
+                              "ok_text": "J'en suis sur",
+                              "dismiss_text": "Annuler"
+                          }
+                      },
+                      {
+                          "text": "Ventre 1 eth",
+                          "type": "button",
+                          "url": "https://www.google.fr",
+                          "confirm": {
+                              "title": "Confirmer la vente",
+                              "text": "Êtes-vous vraiment sur de vouloir vendre 1 eth ?",
+                              "ok_text": "J'en suis sur",
+                              "dismiss_text": "Annuler"
+                          }
+                      },
+                      {
+                          "text": "Afficher l'analyse",
+                          "type": "button",
+                          "url": urlGraph,
+                      }
+                  ]
+              }]
+      };
 
     console.log("urlGraph " + urlGraph);
 
@@ -105,12 +111,11 @@ export class Bot {
             //bot.postMessageToChannel("smart-dev-niort-1-bot", message, Bot.params);
           } else if (botState.isBullish !== jsonBody["isBullish"]) {
             message = talker.generateMessage(jsonBody["isBullish"],jsonBody["acceleration"]);
-            message = message + urlGraph;
             // this.bot.postMessageToChannel("random", message, Bot.params);
             // bot.postMessageToUser(this.slackUser, message, Bot.params);
 
 
-            bot.postMessageToChannel("smart-dev-niort-1-bot", message, Bot.params);
+            bot.postMessageToChannel("smart-dev-niort-1-bot", message, params);
           }
           botState.isBullish = jsonBody["isBullish"];
           botState.acceleration = jsonBody["acceleration"];
